873e1ac6b06532cbbf6cac61f5f21b312ff1fbde
Janet Webster <twixmixy@gmail.com>
2023-10-23 17:10:44 -0400

    Tried to lay out the index

--- index.js
+++ index.js
@@ -3,3 +3,29 @@ const CLI = require('./lib/cli.js');
 const cli = new CLI();
 
 cli.run();
+
+class Component {
+    constructor(text, priority, confirmAddTask) {
+        this.text = text;
+        this.priority = priority;
+        this.confirmAddTask = confirmAddTask;
+    }
+}
+
+class Header extends Component {
+    constructor(text, priority, confirmAddTask) {
+        super(text, priority, confirmAddTask)
+    }
+}
+
+class TaskList extends Component {
+    constructor(text, priority, confirmAddTask) {
+        super(text, priority, confirmAddTask)
+    }
+}
+
+class TaskListItem extends TaskList {
+    constructor(text, priority, confirmAddTask) {
+        super(text, priority, confirmAddTask);
+    }
+}
