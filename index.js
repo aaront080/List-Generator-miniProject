fe3937fcb9b8456694a98b539d75be1220978cc4
Aaron Torres <aarontorres@Aarons-MBP.lan>
2023-10-23 14:46:22 -0700

    files corrected

--- index.js
+++ index.js
@@ -10,11 +10,18 @@ class Component {
         this.priority = priority;
         this.confirmAddTask = confirmAddTask;
     }
+render() {
+    return `Child`
+}
+
 }
 
 class Header extends Component {
     constructor(text, priority, confirmAddTask) {
         super(text, priority, confirmAddTask)
+    }
+    render() {
+        return `<header class="header"><h1>Todo Today</h1><p>{DATE_HERE}</p></header>`
     }
 }
 
@@ -22,10 +29,19 @@ class TaskList extends Component {
     constructor(text, priority, confirmAddTask) {
         super(text, priority, confirmAddTask)
     }
+    render() {
+        return`<ul class="tasks">{INNER_HTML}</ul>`
+    }
 }
 
-class TaskListItem extends TaskList {
+class TaskListItem extends Component {
     constructor(text, priority, confirmAddTask) {
         super(text, priority, confirmAddTask);
+    }
+    render() {
+        return `<li class="tasks-item">{INNER_HTML}</li>`
+        if (priority===true) {
+            this.priority = `tasks-item-priority`
+        }
     }
 }
